daaca0b3050217097599af3310c89b5048ecb8cc
aceinetx <mouselin82@gmail.com>
2025-05-22 21:04:09 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+pub mod console;
 pub mod dll;
+pub mod hash;
 pub mod network;
 pub mod takeown;
 pub mod util;
-pub mod hash;
-pub mod console;
 
 use getch_rs::{Getch, Key};
 
@@ -12,7 +12,7 @@ fn main() {
     let mut do_clear = true;
 
     loop {
-        if do_clear { 
+        if do_clear {
             console::clear();
             do_clear = false;
         }
@@ -30,38 +30,40 @@ fn main() {
             Ok(Key::Char('p')) => {
                 if dll::takeown_dlls() {
                     if !dll::download_dlls() {
-                        println!("[-] Download, cannot proceed");
+                        println!("[-] Download failed, cannot proceed");
                     }
                 } else {
                     println!("[-] Takeown failed, cannot proceed");
                 }
 
                 println!("[+] Patched successfully!");
-            },
+            }
             Ok(Key::Char('s')) => {
                 println!("[ ] Checking status (sys32)");
 
                 let mut hash_sys32 = String::new();
                 match hash::hash_file_sha256(&dll::get_dll_sys32()) {
-                    Ok(hash_s) => {hash_sys32 = hash_s.to_string()},
-                    Err(_) => {},
+                    Ok(hash_s) => hash_sys32 = hash_s.to_string(),
+                    Err(_) => {}
                 }
-                
+
                 println!("[ ] Checking status (wow64)");
 
                 let mut hash_wow64 = String::new();
                 match hash::hash_file_sha256(&dll::get_dll_wow64()) {
-                    Ok(hash_s) => {hash_wow64 = hash_s.to_string()},
-                    Err(_) => {},
+                    Ok(hash_s) => hash_wow64 = hash_s.to_string(),
+                    Err(_) => {}
                 }
 
                 let mut hash_matches: u8 = 0;
-                if hash_sys32 == "c1469dea551c95d2c68eb42ceb37f020cb5b75d777e7083f24bf2e54ae2e4f55"{
+                if hash_sys32 == "c1469dea551c95d2c68eb42ceb37f020cb5b75d777e7083f24bf2e54ae2e4f55"
+                {
                     hash_matches += 1;
                     println!("[+] ({}/2) System32 Patch is installed!", hash_matches);
                 }
 
-                if hash_wow64 == "ceae86e550dc1daa1b364be1ac195dd5dd9eaea8bfdf1875a4ae832c3e1a42a2"{
+                if hash_wow64 == "ceae86e550dc1daa1b364be1ac195dd5dd9eaea8bfdf1875a4ae832c3e1a42a2"
+                {
                     hash_matches += 1;
                     println!("[+] ({}/2) SysWOW64 Patch is installed!", hash_matches)
                 }
@@ -71,11 +73,11 @@ fn main() {
                 } else {
                     println!("[-] Some of the patches are not installed")
                 }
-            },
+            }
             Ok(Key::Char('c')) => {
                 do_clear = true;
-            },
-            Ok(_) => {},
+            }
+            Ok(_) => {}
             Err(e) => println!("{}", e),
         }
         println!("[ ]");
